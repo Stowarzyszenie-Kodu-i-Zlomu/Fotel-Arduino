08fb07dd5a7d09cf0561c83b0c4b308ad254e068
s30274 <s30274@pjwstk.edu.pl>
2025-02-10 22:33:39 +0100

    Grudniowe testy

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -30,6 +30,10 @@ char menuOption = 0;
 long time0;
 int offset = 19;                //dolna granica mierzenia sensora                                                                 
 int skok = 80;                  //skok siłownika
+int deadzone = 5;
+int status = 0; // 0 -> nothign, 1-> extending, -1 -> retracting
+bool isCalibrated = false;
+
 
 // Function definitions
 void setup();
@@ -38,6 +42,10 @@ void ultrasonicSensor();
 int lcDisplay(String line1, String line2);
 void potentiometer();
 void relayModule();
+void start_retract();
+void start_extend();
+void stop_retract();
+void stop_extend();
 
 // Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
 void setup() 
@@ -63,21 +71,51 @@ void setup()
     for (int i = 0; i < 2; i++) { 
       digitalWrite(RelayModule6chPins[i],HIGH);
     }
+   // Serial.print("Distance: ");
+    // Serial.print(ultrasonicDistance());
+    
+
+
 }
 
 // Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
 void loop() 
 {
+  if(!isCalibrated){
+    calibrate();
+    isCalibrated = true;
+  }
   bool retracting = false;
   bool extending = false;
   int target = getTarget();
   int distance = ultrasonicDistance();
   int diff = target-distance;
-
-  // Kod do rozciągania i chowania siłownika
-
   lcdStats(target, distance, retracting, extending);
-  delay(100);
+  delay(500);
+
+  if(distance > target + deadzone && status != -1){
+    stop_extend();
+    start_retract();
+    Serial.println("retracting");
+    status = -1;
+  } else if(distance < target - deadzone && status != 1){
+    stop_retract();
+    start_extend();
+    status = 1;
+    Serial.println("extending");
+  } else if(status != 0) {
+    stop_retract();
+    stop_extend();
+    status = 0;
+    Serial.println("stop");
+  }
+
+  Serial.print("\nTarget: ");
+  Serial.print(target);
+  Serial.print("\nDistance: ");
+  Serial.print(distance);
+  lcdStats(target, distance, retracting, extending);
+  delay(500);
 }
 
 // returns distance in mm as int
@@ -91,7 +129,7 @@ int ultrasonicDistance(){
 
     // send pulses
     digitalWrite(HCSR04_PIN_TRIG, HIGH);  
-    delayMicroseconds(10);
+    delayMicroseconds(1);
 
     // stop sending pulses
     digitalWrite(HCSR04_PIN_TRIG, LOW);
@@ -120,6 +158,37 @@ int ultrasonicDistance(){
     } else {
       return 0;
     }
+}
+
+// na razie void ale potem zwraca cm na sekunde np albo coś w tym stylu
+void calibrate(){
+  int time = 0;
+  int time_after = 0;
+  int time_trys[11];
+  int distance =1;
+  for(int i = 0; i < 2; i++){
+      start_retract();
+      delay(2000);
+      stop_retract();
+      time = millis();
+      start_extend();
+      
+    while(true){
+      distance = ultrasonicDistance();
+      // Serial.println(distance);
+      if(distance >75){
+        stop_extend();
+        time_after = millis();
+        break;
+      }
+    }
+    time_trys[i] = time_after - time;
+  }
+  
+  for(int i=0; i<2; i++){
+    Serial.println(time_trys[i]);
+  }
+
 }
 
 void ultrasonicSensor(){
@@ -174,3 +243,19 @@ void relayModule(){
       delay(700);
     }
  }
+
+void start_retract(){
+  digitalWrite(RelayModule6chPins[0],LOW);
+}
+
+void stop_retract(){
+  digitalWrite(RelayModule6chPins[0],HIGH);
+}
+
+void start_extend(){
+  digitalWrite(RelayModule6chPins[1],LOW);
+}
+
+void stop_extend(){
+  digitalWrite(RelayModule6chPins[1],HIGH);
+}
